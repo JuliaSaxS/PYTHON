bf35dbb4227c0e5f5aa8e6ce71fdff2f811fba9e
julia <sax-s@mail.ru>
2023-04-21 18:11:38 +0300

    Added Task 16

---
+++ HomeWork_Python/Task_16.py
@@ -0,0 +1,23 @@
+# Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N]. 
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+
+# *Пример:*
+
+# 5
+#     1 2 3 4 5
+#     3
+#     -> 1
+
+N = int(input('Enter the number of elements in the array: '))
+enter_N = input('Enter the array elements (using a space between the numbers): ').split()
+arr = list(map(int, enter_N))
+if len(enter_N) != N:
+    print('Input error.')
+else:
+    X = int (input('Enter the number X: '))
+    count = 0
+    for i in range(N):
+        if arr[i] == X:
+            count += 1
+print(f'The number {X} occurs in the array A {count} times.')
