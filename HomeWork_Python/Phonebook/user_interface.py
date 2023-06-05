e25f0bb400f3793520263b9b1daa6ea3f5331d1c
JuliaSaxS <120261142+JuliaSaxS@users.noreply.github.com>
2023-06-05 15:53:34 +0300

    Added changes

--- HomeWork_Python/Phonebook/user_interface.py
+++ HomeWork_Python/Phonebook/user_interface.py
@@ -8,10 +8,10 @@ def get_info():
     info.append(second_name)
     phone_number = input('Введите номер телефона: ')
     while len(phone_number) < 11:
-        phone_number = input('Количествво символов не верное. Повторите ввод: ')
+        phone_number = input('Количествво символов неверное. Повторите ввод: ')
     for i in phone_number:
         if i not in '-()0123456789 ':
-            phone_number = input(f'Номер введен не верно (символ ({i})). Повторите ввод: ')
+            phone_number = input(f'Номер введен неверно (символ ({i})). Повторите ввод: ')
     info.append(phone_number)
     description = input('Введите описание: ')
     info.append(description)
